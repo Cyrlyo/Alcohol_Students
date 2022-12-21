4a2e19a4f1791f9a7816b46b9252f59449fa4a5a
Marin <marinmainka@gmail.com>
2022-12-21 20:12:14 +0100

    refactoring script

--- StudentAlcool.py
+++ StudentAlcool.py
@@ -1,76 +1,24 @@
-import pandas as pd
 import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
-import collections
 import community.community_louvain as community_louvain
 from mpl_toolkits.mplot3d import Axes3D
 import pyvis
 import random
-from statistics import median
 from pandas import DataFrame
 from numpy import ndarray
 from networkx.classes.graph import Graph
 from tqdm import tqdm
-from collections import defaultdict
 import os
 from typing import List,  Tuple
-import yaml
-from yaml.loader import SafeLoader
 import time
-import argparse
-
-def importData(path: str) -> DataFrame:
-    
-    data = pd.read_csv(path)
-    return data
-
-def prepareData(data: DataFrame) -> DataFrame:
-    
-    data.rename(columns={"sex":"gender"}, inplace=True)
-    
-    try: 
-        data.drop(columns='Unnamed: 0', inplace=True)
-    except: pass
-    
-    data['alc'] = data['Dalc'] + data['Walc']
-    data["guardian"][data["guardian"] == "father"] = "parent"
-    data["guardian"][data["guardian"] == "mother"] = "parent"
-    data['absences'][(data["absences"] > 0) & (data["absences"] < 11)] = 1
-
-    data = createName(data)
-    return data
-
-def createName(data: DataFrame) -> DataFrame:
-    
-    indexes = [i for i in range(data.shape[0])]
-    indexes = pd.DataFrame({"Name":indexes})
-    data = pd.concat([data, indexes], axis=1)
-    
-    return data
-
-def printDataInfos(data: DataFrame):
-    
-    print("\n************************\n")
-    print(f"\nShape: {data.shape}\n")
-    print(f"Columns:\n{data.columns}")
-    print(data.head(3))
-    print("\n************************\n")
+from utilities import *
 
 def DFToNP(data: DataFrame) -> ndarray:
     data_vec = data.to_numpy()
     print(f"\nOriginal shape: {data.shape} | New shape: {data_vec.shape}\n")
     return data_vec
-
-def printScoresStats(list_of_scores: list):
-    
-    print("\nDifference score statistics:")
-    print(f"Len: {len(list_of_scores)}")
-    print(f"Mean: {round(np.mean(list_of_scores), 4)}")
-    print(f"Median: {round(np.median(list_of_scores), 4)}")
-    print(f"Max: {round(np.max(list_of_scores), 4)}")
-    print(f"Min: {round(np.min(list_of_scores), 4)}\n")
 
 def createGraph(G: Graph, data: DataFrame, data_vec: ndarray, reuse: bool = True, random_weights: bool = True) -> Tuple[Graph, dict]:
     
@@ -105,7 +53,6 @@ def prepareGraph(G: Graph) -> Graph:
     G.remove_edges_from(nx.selfloop_edges(G))
     return G
 
-
 def plotGraphStats(G: Graph):
     
     print(f"\nNumber of nodes: {G.number_of_nodes()}")
@@ -120,32 +67,12 @@ def randomWeights(data: DataFrame, reuse: bool) -> dict:
     weights["Name"] = 0
     return weights
 
-def graphPlot(G: Graph):
-    
-    plt.figure(figsize=(12, 8))
-    graph_options = {
-        'node_color': 'lightblue',
-        'node_size' : 10,
-        "edge_color": 'grey'
-    }
-
-    nx.draw(G, **graph_options, label=True)
-    plt.show()
 
 def louvainPartitioning(G: Graph) -> dict:
     
     partition = community_louvain.best_partition(G)
     print(f"\nNumber of partitions: {len(set(partition.values()))}")
     return partition
-
-def plotGraphWithPartition(G: Graph, partition: dict):
-    
-    plt.figure(figsize=(12, 8))
-    pos = nx.spring_layout(G)
-    cmap = cm.get_cmap('viridis', max(partition.values()) + 1)
-    nx.draw_networkx_nodes(G, pos, partition.keys(), node_size=40, cmap=cmap, node_color=list(partition.values()))
-    nx.draw_networkx_edges(G, pos, alpha=0.5)
-    plt.show()
 
 def louvain_community_quality(G, communities):
     """
@@ -162,54 +89,6 @@ def louvain_community_quality(G, communities):
     print(f"Modularity for this graph: {round(modularity, 4)}\n")
     
     return modularity
-
-def refactoringPartition(partition: dict) -> list:
-
-    sets = defaultdict(set)
-    for key, value in partition.items():
-        sets[value].add(key)
-    part_by_com = [sets[x] for x in list(sets.keys())]
-    
-    return part_by_com
-
-def sortedPartition(partition: dict) -> dict:
-    
-    partition_sorted = dict(sorted(partition.items()))
-    return partition_sorted
-
-def addPartitionToData(data: DataFrame, partition: dict) -> DataFrame:
-    
-    partition_sorted = sortedPartition(partition)
-    partition_sorted = pd.DataFrame.from_dict(partition_sorted, orient="index")
-    data = pd.concat([data, partition_sorted], axis=1)
-    data.rename({0:"Community"}, axis=1, inplace=True)
-    
-    return data
-
-def saveDFToCSV(data: DataFrame):
-    
-    checkExistingFolder("./Data")    
-    data.to_csv("./Data/student_all_community.csv", sep=",", index=False)
-
-def checkExistingFolder(path: str):
-    
-    result = os.path.exists(path)
-    if not result:
-        os.mkdir(path)
-    
-def saveWeights(weights: dict, path: str):
-    
-    checkExistingFolder(path)
-    full_path = os.path.join(path, "weights.yaml")
-    
-    with open(full_path, "w") as file:
-        yaml.dump(weights, file, default_flow_style=False)
-
-def loadWeights(path: str) -> dict:
-    
-    with open(path, "r") as file:
-        weights = yaml.load(file, Loader=SafeLoader)
-    return weights
 
 def findBestRandomWeight(data: DataFrame, data_vec: ndarray) -> Tuple[dict, ndarray]:
     
@@ -254,17 +133,7 @@ def findBestRandomWeight(data: DataFrame, data_vec: ndarray) -> Tuple[dict, ndar
         print(f"Execution time: {time.strftime('%H:%M:%S', time.gmtime(delta_time))}")
     return results, np.array(score_list)
 
-def saveScore(score: float):
-    
-    checkExistingFolder("./weights")
-    with open("./weights/best_score.txt", "w") as file:
-        file.write(str(score))
 
-def loadScore(path: str):
-    
-    with open("./weights/best_score.txt", "r") as file:
-        best_score = file.read()
-    return best_score
 
 if __name__ == "__main__":
     
---
+++ utilities.py
@@ -0,0 +1,139 @@
+import os
+import pandas as pd
+from pandas import DataFrame
+import yaml 
+from yaml.loader import SafeLoader
+from collections import defaultdict
+import numpy as np
+import networkx as nx
+from networkx.classes.graph import Graph
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+
+def saveScore(score: float):
+    
+    checkExistingFolder("./weights")
+    with open("./weights/best_score.txt", "w") as file:
+        file.write(str(score))
+
+def loadScore(path: str):
+    
+    with open("./weights/best_score.txt", "r") as file:
+        best_score = file.read()
+    return best_score
+
+def saveDFToCSV(data: DataFrame):
+    
+    checkExistingFolder("./Data")    
+    data.to_csv("./Data/student_all_community.csv", sep=",", index=False)
+
+def checkExistingFolder(path: str):
+    
+    result = os.path.exists(path)
+    if not result:
+        os.mkdir(path)
+    
+def saveWeights(weights: dict, path: str):
+    
+    checkExistingFolder(path)
+    full_path = os.path.join(path, "weights.yaml")
+    
+    with open(full_path, "w") as file:
+        yaml.dump(weights, file, default_flow_style=False)
+
+def loadWeights(path: str) -> dict:
+    
+    with open(path, "r") as file:
+        weights = yaml.load(file, Loader=SafeLoader)
+    return weights
+
+def sortedPartition(partition: dict) -> dict:
+    
+    partition_sorted = dict(sorted(partition.items()))
+    return partition_sorted
+
+def refactoringPartition(partition: dict) -> list:
+
+    sets = defaultdict(set)
+    for key, value in partition.items():
+        sets[value].add(key)
+    part_by_com = [sets[x] for x in list(sets.keys())]
+    
+    return part_by_com
+
+def importData(path: str) -> DataFrame:
+    
+    data = pd.read_csv(path)
+    return data
+
+def prepareData(data: DataFrame) -> DataFrame:
+    
+    data.rename(columns={"sex":"gender"}, inplace=True)
+    
+    try: 
+        data.drop(columns='Unnamed: 0', inplace=True)
+    except: pass
+    
+    data['alc'] = data['Dalc'] + data['Walc']
+    data["guardian"][data["guardian"] == "father"] = "parent"
+    data["guardian"][data["guardian"] == "mother"] = "parent"
+    data['absences'][(data["absences"] > 0) & (data["absences"] < 11)] = 1
+
+    data = createName(data)
+    return data
+
+def printScoresStats(list_of_scores: list):
+    
+    print("\nDifference score statistics:")
+    print(f"Len: {len(list_of_scores)}")
+    print(f"Mean: {round(np.mean(list_of_scores), 4)}")
+    print(f"Median: {round(np.median(list_of_scores), 4)}")
+    print(f"Max: {round(np.max(list_of_scores), 4)}")
+    print(f"Min: {round(np.min(list_of_scores), 4)}\n")
+    
+def createName(data: DataFrame) -> DataFrame:
+    
+    indexes = [i for i in range(data.shape[0])]
+    indexes = pd.DataFrame({"Name":indexes})
+    data = pd.concat([data, indexes], axis=1)
+    
+    return data
+
+def addPartitionToData(data: DataFrame, partition: dict) -> DataFrame:
+    
+    partition_sorted = sortedPartition(partition)
+    partition_sorted = pd.DataFrame.from_dict(partition_sorted, orient="index")
+    data = pd.concat([data, partition_sorted], axis=1)
+    data.rename({0:"Community"}, axis=1, inplace=True)
+    
+    return data
+
+def plotGraphWithPartition(G: Graph, partition: dict):
+    
+    plt.figure(figsize=(12, 8))
+    pos = nx.spring_layout(G)
+    cmap = cm.get_cmap('viridis', max(partition.values()) + 1)
+    nx.draw_networkx_nodes(G, pos, partition.keys(), node_size=40, cmap=cmap, node_color=list(partition.values()))
+    nx.draw_networkx_edges(G, pos, alpha=0.5)
+    plt.show()
+    
+
+def printDataInfos(data: DataFrame):
+    
+    print("\n************************\n")
+    print(f"\nShape: {data.shape}\n")
+    print(f"Columns:\n{data.columns}")
+    print(data.head(3))
+    print("\n************************\n")
+    
+def graphPlot(G: Graph):
+    
+    plt.figure(figsize=(12, 8))
+    graph_options = {
+        'node_color': 'lightblue',
+        'node_size' : 10,
+        "edge_color": 'grey'
+    }
+
+    nx.draw(G, **graph_options, label=True)
+    plt.show()
