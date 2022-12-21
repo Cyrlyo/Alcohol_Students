f52c8eb4c93ba305e83b65a479abc0d22017158b
Marin <marinmainka@gmail.com>
2022-12-21 13:09:41 +0100

    Add script

---
+++ StudentAlcool.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import collections
+import community.community_louvain as community_louvain
+from mpl_toolkits.mplot3d import Axes3D
+import pyvis
+import random
+from statistics import median
+
