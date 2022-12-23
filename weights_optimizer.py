7755bb96554054723d63708f30ba2b7adf08a599
Marin <marinmainka@gmail.com>
2022-12-23 20:14:43 +0100

    deleting pandas error old code still in coments

--- weights_optimizer.py
+++ weights_optimizer.py
@@ -14,14 +14,15 @@ if __name__ == "__main__" :
     num_boost_round = 100
     nfold = 5
     
-    data_dmatrix = xgb.DMatrix(data=data[data.columns[:-2]], label=data["alc"], enable_categorical=True)
-    xgb_cv = xgb.cv(dtrain=data_dmatrix, params={'objective':'reg:squarederror'}, nfold=nfold, metrics = 'rmse', seed=42, num_boost_round=num_boost_round)
-    
-    # Train the model
-    num_boost_round = 20
-    clf = xgb.train({'objective':'reg:squarederror', 'eval_metric': 'rmse', 'seed': 42}, data_dmatrix, num_boost_round=num_boost_round)
-    
-    # Plot feature importance
-    xgb.plot_importance(clf)
-    plt.figure(figsize = (16, 12))
-    plt.show()
+    if False:
+        data_dmatrix = xgb.DMatrix(data=data[data.columns[:-2]], label=data["alc"], enable_categorical=True)
+        xgb_cv = xgb.cv(dtrain=data_dmatrix, params={'objective':'reg:squarederror'}, nfold=nfold, metrics = 'rmse', seed=42, num_boost_round=num_boost_round)
+        
+        # Train the model
+        num_boost_round = 20
+        clf = xgb.train({'objective':'reg:squarederror', 'eval_metric': 'rmse', 'seed': 42}, data_dmatrix, num_boost_round=num_boost_round)
+        
+        # Plot feature importance
+        xgb.plot_importance(clf)
+        plt.figure(figsize = (16, 12))
+        plt.show()
