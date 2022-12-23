2bab32fdb4abb5f38606926868c1304dd546f4e0
Marin <marinmainka@gmail.com>
2022-12-23 20:03:13 +0100

    create dmatrix

--- weights_optimizer.py
+++ weights_optimizer.py
@@ -1,4 +1,17 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import xgboost as xgb
+# import xgboost as xgb
+from utilities import importData, prepareData
+
+
+
+if __name__ == "__main__" :
+    
+    data = importData("./Data/student_all.csv")
+    data = prepareData(data)
+    
+    num_boost_round = 100
+    nfold = 5
+    
+    # data_dmatrix = xgb.DMatrix(data=data[data.columns[:-2]], label=data["alc"], enable_categorical=True)
