c8b64c2ffaf218d8b07b6af426296b6fa1dd7c77
Marin <marinmainka@gmail.com>
2022-12-24 18:41:44 +0100

    refactoring wiehgts optimizer

--- weights_optimizer.py
+++ weights_optimizer.py
@@ -6,7 +6,7 @@ import xgboost as xgb
 from xgboost import Booster
 from utilities import importData, prepareData
 
-NUM_BOOST_ROUND = 100
+NUM_BOOST_ROUND = 20
 NFOLD = 5
 
 
@@ -22,7 +22,6 @@ def changeDataCategory(data: DataFrame) -> DataFrame:
     return data
 
 def reduceValueRange(x: float) -> float:
-    #TODO: rename this function
   return (1 / (1 + np.exp(-x)))*2
 
 def XGBoostClassification(data: DataFrame, num_boost_round: int, nfold: int) -> Booster:
@@ -46,19 +45,15 @@ def plotFeatureImportance(model: Booster) -> None:
     xgb.plot_importance(model)
     plt.show()
 
+
+
 if __name__ == "__main__" :
     
     data = importData("./Data/student_all.csv")
     data = prepareData(data)
     data = changeDataCategory(data)
 
-
-    # plt.figure(figsize=(14, 8))
-    # plt.errorbar(xgb_cv.index, xgb_cv['train-rmse-mean'], yerr=xgb_cv['train-rmse-std'], label='train_rmse')
-    # plt.errorbar(xgb_cv.index, xgb_cv['test-rmse-mean'], yerr=xgb_cv['test-rmse-std'], label='valid_rmse')
-    # plt.legend()
-    # plt.title(f'Evolution of the train and validation mean and standard deviation RMSE over {nfold} cross-validation folds as a function of the round number')
-    # plt.xlabel('Round number')
-    # plt.ylabel('Mean RMSE')
-    # plt.show()
+    model = XGBoostClassification(data, NUM_BOOST_ROUND, NFOLD)
+    weights_xgboost = featureImportance(model)
+    
     
